1fb43e74c53a24c483c4234be16865e11988cd9b
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-29 15:37:47 +0300

    fix: испарвлено 2 замечаниz ревьюера

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/Hit.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/Hit.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -16,11 +18,15 @@ public class Hit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @Column(name = "app")
+    @NotBlank
     String app;
     @Column(name = "uri")
+    @NotBlank
     String uri;
     @Column(name = "ip")
+    @NotBlank
     String ip;
     @Column(name = "created")
+    @NotNull
     LocalDateTime timestamp;
 }
